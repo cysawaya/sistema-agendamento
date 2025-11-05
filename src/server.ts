05bb173542e56e52326d8b733134506e3c95e5ed
cysawaya <cynthiasawaya.ads@gmail.com>
2025-11-05 20:19:29 -0300

    Sua mensagem

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import routers from './routers./index.ts';
+import routers from 'inspector';
 
 const app = express();
 app.use(express.json());
-app.use('/api', routers);
+app.use('/api');
 
 export default app;
 
